3a68f4971f2061c9ffb31cf21808ba4fb445107a
Ioan Velev <ioanvelev46@gmail.com>
2025-01-15 17:08:19 +0200

    Create ErrorMessage.vue

---
+++ vue-app/src/components/ErrorMessage.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+props: {
+    errors: {
+        type: Array,
+        required: true,
+    }
+}
+}
+</script>
+
+<template>
+<div class="err-message">
+    <strong
+        v-for="error of errors"
+        :key="error.$uid"
+      >
+        {{ error.$message }}
+      </strong>
+</div>
+</template>
+
+<style scoped>
+.err-message {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+  font-size: 0.75rem;
+  font-weight: bold;
+  color: red;
+}
+</style>

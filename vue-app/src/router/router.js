f8ee3d7eee8e1a3c36299f5704afbbb00b5f925e
Ioan Velev <ioanvelev46@gmail.com>
2025-01-15 15:22:08 +0200

    Update router.js

--- vue-app/src/router/router.js
+++ vue-app/src/router/router.js
@@ -1,11 +1,15 @@
 import AboutPage from '@/components/AboutPage.vue'
 import Home from '@/components/Home.vue'
+import LoginPage from '@/components/LoginPage.vue';
+import RegisterPage from '@/components/RegisterPage.vue';
 import { createWebHistory, createRouter } from 'vue-router'
 
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: AboutPage },
+  { path: '/register', component: RegisterPage },
+  { path: '/login', component: LoginPage},
 ]
 
 const router = createRouter({

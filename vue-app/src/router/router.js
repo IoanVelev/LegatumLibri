63c5d31a0672de4cd22ab328ae1d59c816b7ba98
Ioan Velev <ioanvelev46@gmail.com>
2025-01-16 18:04:21 +0200

    Update router.js

--- vue-app/src/router/router.js
+++ vue-app/src/router/router.js
@@ -1,6 +1,7 @@
 import AboutPage from '@/components/AboutPage.vue'
 import BookDetails from '@/components/BookDetails.vue';
 import BookForm from '@/components/BookForm.vue';
+import BooksFavourites from '@/components/BooksFavourites.vue';
 import BooksWishlist from '@/components/BooksWishlist.vue';
 import Home from '@/components/Home.vue'
 import LoginPage from '@/components/LoginPage.vue';
@@ -15,7 +16,8 @@ const routes = [
   { path: '/login', component: LoginPage },
   { path: '/create', component: BookForm },
   { path: '/book/:id', component: BookDetails },
-  { path: '/wishlist', component: BooksWishlist }
+  { path: '/wishlist', component: BooksWishlist },
+  { path: '/favourites', component: BooksFavourites }
 ]
 
 const router = createRouter({
